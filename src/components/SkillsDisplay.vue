768c97e5cba962b0e3f0834fb9f77ee33628eaec
XavierMolyneaux <xmolyneaux@gmail.com>
2024-02-29 18:44:41 -0600

    Skills page initial setup.

---
+++ src/components/SkillsDisplay.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="flex flex-col mt-6 sm:flex-row">
+        <div class="flex flex-row sm:flex-col">
+            <button class="px-4 py-3 selector-bg-hover left-border-one text-left text-lg sm:border-l-2 sm:border-b-0 border-l-0 border-b-2 transition ease-in-out delay-100 translate font-bold"
+                v-for="(skill, index) in skillTypes"
+                :key="index"
+                @click="currentSelectedTechnology(index)">
+                {{ skill }}
+            </button>
+        </div>
+
+        <div class="flex flex-row sm:flex-col"
+            v-for="(skills, index) in skillsData"
+            :key="index">
+            <div class=""
+                v-if="skills.type == selectedSkill">
+                {{ skills.technologies }}
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import SkillsData from './SkillsDisplay.json'
+
+export default {
+    name: 'SkillsDisplay',
+    components: {},        
+    props: {
+        skillTypes: {
+            type: Array,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            skillsData: SkillsData,
+            selectedSkill: this.currentSelectedTechnology(0)
+        }
+    },
+
+    mounted() {
+		this.currentSelectedTechnology(0);
+	},
+
+	methods: {
+		currentSelectedTechnology(index) {
+			switch (index) {
+			case 0:
+				this.selectedSkill = 'Frontend';
+				break;
+			case 1:
+				this.selectedSkill = 'Backend';
+				break;
+			case 2:
+				this.selectedSkill = 'Infrastructure';
+				break;
+			}
+		}
+    }    
+}
+</script>
---
+++ src/components/TechCards.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <p>Hello!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: TechCards,
+    props: {
+        technologies: {
+            type: Array,
+            required: true
+        },
+    
+    }
+}
+</script>
